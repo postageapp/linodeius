b5d6829258b4c2e3e15dabc0d26b07c0506f7fa9
Scott Tadman <tadman@postageapp.com>
2016-06-14 16:33:54 -0400

    Sketching in Linode interface, adding sample method files

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@ var gulp = require('gulp');
 
 var Writable = require('stream').Writable;
 
-gulp.task('methods', function() {
+var methods = require('./methods');
+
+gulp.task('clean', function() {
+  methods.clean();
+})
+
+gulp.task('update', function() {
   var api_scraper = require('./support/api-scraper');
 
   api_scraper().pipe(new Writable({
     objectMode: true,
     write: function(obj, encoding, callback) {
-      console.log(obj);
-      callback();
+      console.log(obj.name);
+
+      methods.save(obj.name, obj).asCallback(callback);
     }
   }));
 });
+
+gulp.task('default', [ 'clean', 'update' ]);
