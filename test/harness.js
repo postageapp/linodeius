490aa606d621f7ad5086649933b79989eb417b2b
Scott Tadman <tadman@postageapp.com>
2016-06-17 15:09:55 -0400

    Reworked interface with API specification pulled down dynamically on initialization

---
+++ test/harness.js
@@ -0,0 +1,5 @@
+// == Imports ===============================================================
+
+require('mocha');
+
+global.assert = require('chai').assert;
---
+++ test/test_api_avail.js
@@ -0,0 +1,18 @@
+require('./harness');
+
+const Linode = require('../lib/linodeius');
+
+it('should return an API specification', function() {
+  var api = new Linode();
+
+  return api.then(function() {
+    assert.ok(api.api);
+    assert.ok(api.api.spec);
+
+    return api.api.spec().then(function(spec) {
+      assert.ok(spec);
+
+      assert.equal(spec.data.version, )
+    });
+  });
+});
